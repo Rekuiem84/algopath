f0c1be7acacc5630f9e5227ca2e0b44a49612bfc
Rekuiem84 <morinijoshua@gmail.com>
2024-12-05 11:17:39 +0100

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,50 @@
+const registerDoc = document.querySelector(".register");
+const nameInput = registerDoc.querySelector("#name");
+const form = registerDoc.querySelector("form");
+const playersList = document.querySelector(".players-list");
+
+const players = [];
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const name = nameInput.value;
+	if (name.trim() && players.length < 4) {
+		players.push(name);
+		localStorage.setItem("players", JSON.stringify(players));
+		renderPlayers();
+		nameInput.value = "";
+		console.log(JSON.parse(localStorage.getItem("players")));
+	}
+});
+
+function renderPlayers() {
+	playersList.innerHTML = "";
+	players.forEach((player) => {
+		const li = document.createElement("li");
+		const removeButton = document.createElement("button");
+		removeButton.classList.add("button-remove");
+		li.textContent = player;
+		removeButton.innerHTML = '<i class="bx bx-x"></i>';
+		removeButton.addEventListener("click", () => {
+			removePlayer(player);
+		});
+		li.appendChild(removeButton);
+		playersList.appendChild(li);
+	});
+}
+
+function removePlayer(player) {
+	const index = players.indexOf(player);
+	if (index > -1) {
+		players.splice(index, 1);
+		localStorage.setItem("players", JSON.stringify(players));
+		renderPlayers();
+	}
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	if (localStorage.getItem("players")) {
+		players.push(...JSON.parse(localStorage.getItem("players")));
+		renderPlayers();
+	}
+});
